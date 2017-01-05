fc439328d74fb0127e525edd1cbce723b5c1952f
Giancarlo Corzo <gian.corzo@gmail.com>
2017-01-05 01:03:08 -0500

    Fixing Figaro reference

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     enable_starttls_auto: true,
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => Figaro.env.smtp_domain }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
